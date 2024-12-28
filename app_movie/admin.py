2c220497a2d8476e043379e476c9ff824ad1ea19
SATYAM NAG <satyamnag.k@gmail.com>
2024-12-28 20:32:09 +0530

    minor change in admin

--- app_movie/admin.py
+++ app_movie/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 from .models import Movie
 
 # Register your models here.
-admin.site.register(Movie)
+
+admin.site.site_header='Movies'
+admin.site.site_title='Movies'
+admin.site.index_title='Manage Movies'
+
+class MovieAdmin(admin.ModelAdmin):
+
+    def user_admin(self, request, queryset):
+        queryset.update(user_name="Site Admin")
+
+    user_admin.short_description="Update User Name to Site Admin"
+    list_display=('movie_name', 'user_name', 'movie_category', 'movie_releaseyear', 'movie_certificate', 'movie_duration', 'movie_rating', 'movie_review', 'movie_budget', 'movie_boxoffice')
+    search_fields=('movie_name',)
+    actions=('user_admin',)
+
+admin.site.register(Movie, MovieAdmin)
